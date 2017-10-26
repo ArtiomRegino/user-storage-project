94b2399bf2ceb9f26c9b1093474c793dbedd2d50
Artiom Regino <artiom.regino@gmail.com>
2017-10-27 01:09:36 +0300

    Add implementation for Add, Remove and Search methods. Add logging.

--- UserStorage/UserStorageServices.Tests/UserStorageServiceTests.cs
+++ UserStorage/UserStorageServices.Tests/UserStorageServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UserStorageServices.Tests
@@ -36,8 +38,400 @@ namespace UserStorageServices.Tests
         }
 
         [TestMethod]
-        public void Remove_WithoutArguments_NothingHappen()
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserFirstNameConsistsOfWhiteSpaces_ExceptionThrown()
         {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                FirstName = "    "
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserFirstNameIsEmptyString_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                FirstName = string.Empty
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserLastNameIsNull_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                LastName = null
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserLastNameConsistsOfWhiteSpaces_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                LastName = "    "
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserLastNameIsEmptyString_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                LastName = string.Empty
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_UserAgeIsLessThanZero_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                Age = -2
+            });
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void Add_StandartUser_NothingHappen()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Add(new User
+            {
+                FirstName = "Lorem",
+                LastName = "Ipsum",
+                Age = 23
+            });
+
+            // Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Remove_NullAsUserArgument_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.Remove(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void Remove_UserNotExist_ReturnedFalse()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act and Assert
+            Assert.IsFalse(userStorageService.Remove(new User()));
+        }
+
+        [TestMethod]
+        public void Remove_CorrectUser_ReturnedTrue()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            var user = new User()
+            {
+                Age = 21,
+                FirstName = "Anna",
+                LastName = "Shprot"
+            };
+            userStorageService.Add(user);
+
+            // Assert 
+            Assert.IsTrue(userStorageService.Remove(user));            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SearchByAge_ArgumentLessThanZero_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByAge(-3);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void SearchByAge_NoSuchAUser_Returned()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act and Assert 
+            Assert.IsInstanceOfType(userStorageService.SearchByAge(3), typeof(IEnumerable<User>));
+        }
+
+        [TestMethod]
+        public void SearchByAge_SearchForAnExistingUser_ReturnedIEnumerableWithCurrentUser()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+            var user = new User()
+            {
+                Age = 27,
+                FirstName = "Michael",
+                LastName = "Fresko"
+            };
+            userStorageService.Add(user);
+
+            // Act and Assert 
+            Assert.AreEqual(user, userStorageService.SearchByAge(27).FirstOrDefault());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SearchByLastName_ArgumentIsNull_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByLastName(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SearchByLastName_ArgumentIsWhiteSpace_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByLastName("   ");
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void SearchByLastName_NoSuchAUser_ReturnedEmptyIEnumerable()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            Assert.IsInstanceOfType(userStorageService.SearchByLastName("Freeman"), typeof(IEnumerable<User>));
+
+            // Assert 
+        }
+
+        [TestMethod]
+        public void SearchByLastName_SearchForAnExistingUser_Returned()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+            var user = new User()
+            {
+                Age = 27,
+                FirstName = "Michael",
+                LastName = "Fresko"
+            };
+            userStorageService.Add(user);
+
+            // Act
+            Assert.AreEqual(user, userStorageService.SearchByLastName("Fresko").FirstOrDefault());
+
+            // Assert 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SearchByFirstName_ArgumentIsNull_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByFirstName(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SearchByFirstName_ArgumentIsWhiteSpace_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByFirstName("   ");
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void SearchByFirstName_NoSuchAUser_ReturnedEmptyIEnumerable()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            Assert.IsInstanceOfType(userStorageService.SearchByFirstName("Morgan"), typeof(IEnumerable<User>));
+
+            // Assert 
+        }
+
+        [TestMethod]
+        public void SearchByFirstName_SearchForAnExistingUser_ReturnedIEnumerableWithCurrentUser()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+            var user = new User()
+            {
+                Age = 27,
+                FirstName = "Michael",
+                LastName = "Fresko"
+            };
+            userStorageService.Add(user);
+
+            // Act
+            Assert.AreEqual(user, userStorageService.SearchByFirstName("Michael").FirstOrDefault());
+
+            // Assert 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SearchByPredicate_ArgumentIsNull_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchByPredicate(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void SearchByPredicate_NoSuchAUser_ReturnedEmptyIEnumerable()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            Assert.IsInstanceOfType(userStorageService.SearchByPredicate(u => u.Age == 11), typeof(IEnumerable<User>));
+
+            // Assert 
+        }
+
+        [TestMethod]
+        public void SearchByPredicate_SearchForAnExistingUser_ReturnedIEnumerableWithCurrentUser()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+            var user = new User()
+            {
+                Age = 27,
+                FirstName = "Michael",
+                LastName = "Fresko"
+            };
+            userStorageService.Add(user);
+
+            // Act
+            Assert.AreEqual(user, userStorageService.SearchByPredicate(u => u.Age == 27).FirstOrDefault());
+
+            // Assert 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SearchFirstByPredicate_ArgumentIsNull_ExceptionThrown()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            userStorageService.SearchFirstByPredicate(null);
+
+            // Assert - [ExpectedException]
+        }
+
+        [TestMethod]
+        public void SearchFirstByPredicate_NoSuchAUser_ReturnedNull()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+
+            // Act
+            Assert.IsNull(userStorageService.SearchFirstByPredicate(u => u.Age == 11));
+
+            // Assert 
+        }
+
+        [TestMethod]
+        public void SearchFirstByPredicate_SearchForAnExistingUser_ReturnedCurrentUser()
+        {
+            // Arrange
+            var userStorageService = new UserStorageService();
+            var user = new User()
+            {
+                Age = 27,
+                FirstName = "Michael",
+                LastName = "Fresko"
+            };
+            userStorageService.Add(user);
+
+            // Act
+            Assert.AreEqual(user, userStorageService.SearchFirstByPredicate(u => u.Age == 27));
+
+            // Assert 
         }
     }
 }
--- UserStorage/UserStorageServices/UserStorageService.cs
+++ UserStorage/UserStorageServices/UserStorageService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UserStorageServices
 {
@@ -7,11 +9,26 @@ namespace UserStorageServices
     /// </summary>
     public class UserStorageService
     {
+        public readonly List<User> Users;
+
+        /// <summary>
+        /// Create an instance of <see cref="UserStorageService"/>
+        /// </summary>
+        public UserStorageService()
+        {
+            Users = new List<User>();
+        }
+
+        public bool IsLoggingEnabled { get; set; }
+
         /// <summary>
         /// Gets the number of elements contained in the storage.
         /// </summary>
         /// <returns>An amount of users in the storage.</returns>
-        public int Count { get; }
+        public int Count
+        {
+            get { return Users.Count; }
+        }
 
         /// <summary>
         /// Adds a new <see cref="User"/> to the storage.
@@ -19,33 +36,139 @@ namespace UserStorageServices
         /// <param name="user">A new <see cref="User"/> that will be added to the storage.</param>
         public void Add(User user)
         {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("Add() method is called.");
+            }
+
             if (user == null)
             {
                 throw new ArgumentNullException(nameof(user));
             }
 
-            if (string.IsNullOrWhiteSpace(user.FirstName))
+            if (string.IsNullOrWhiteSpace(user.LastName) || string.IsNullOrWhiteSpace(user.FirstName))
             {
-                throw new ArgumentException("FirstName is null or empty or whitespace", nameof(user));
+                throw new ArgumentException("LastName or FirstName is null or empty or whitespace", nameof(user));
             }
 
-            // TODO: Implement Add() method and all other validation rules.
+            if (user.Age < 0)
+            {
+                throw new ArgumentException("Age is null less or equal to 0.", nameof(user));
+            }
+
+            Users.Add(user);
         }
 
         /// <summary>
         /// Removes an existed <see cref="User"/> from the storage.
         /// </summary>
-        public void Remove()
+        public bool Remove(User user)
         {
-            // TODO: Implement Remove() method.
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("Remove() method is called.");
+            }
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            return Users.Remove(user); 
+        }
+
+        /// <summary>
+        /// Searches through the storage for a <see cref="IEnumerable&lt;User&gt;"/> that have such an age.
+        /// </summary>
+        public IEnumerable<User> SearchByAge(int age)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByAge() method is called.");
+            }
+
+            if (age <= 0)
+            {
+                throw new ArgumentException("Age can't be less than 0.", nameof(age));
+            }
+
+            return SearchByPredicate(u => age == u.Age);
+        }
+
+        /// <summary>
+        /// Searches through the storage for a <see cref="IEnumerable&lt;User&gt;"/> that have such a last name.
+        /// </summary>
+        public IEnumerable<User> SearchByLastName(string lastName)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByLastName() method is called.");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException("FirstName is null or empty or whitespace", nameof(lastName));
+            }
+
+            return SearchByPredicate(u => lastName == u.LastName);
+        }
+
+        /// <summary>
+        /// Searches through the storage for a <see cref="IEnumerable&lt;User&gt;"/> that have such a first name.
+        /// </summary>
+        public IEnumerable<User> SearchByFirstName(string firstName)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByFirstName() method is called.");
+            }
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("FirstName is null or empty or whitespace", nameof(firstName));
+            }
+
+            return SearchByPredicate(u => firstName == u.FirstName);
         }
 
         /// <summary>
         /// Searches through the storage for a <see cref="User"/> that matches specified criteria.
         /// </summary>
-        public void Search()
+        public User SearchFirstByPredicate(Predicate<User> predicate)
         {
-            // TODO: Implement Search() method.
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchFirstByPredicate() method is called.");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var currentUsers = Users.FirstOrDefault(u => predicate(u));
+
+            return currentUsers;
+        }
+
+        /// <summary>
+        /// Searches through the storage for a <see cref="User"/> that matches specified criteria.
+        /// </summary>
+        public IEnumerable<User> SearchByPredicate(Predicate<User> predicate)
+        {
+            if (IsLoggingEnabled)
+            {
+                Console.WriteLine("SearchByPredicate() method is called.");
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var currentUsers = Users.Where(u => predicate(u));
+
+            return currentUsers;
         }
     }
 }
